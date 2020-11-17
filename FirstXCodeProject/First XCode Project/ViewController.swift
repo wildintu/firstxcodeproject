92abe67785af17aeefb13ba450de6da860594a43
Lexi <54863484+wildintu@users.noreply.github.com>
2020-11-17 17:58:26 -0600

    Swift - First Xcode Project

---
+++ FirstXCodeProject/First XCode Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First XCode Project
+//
+//  Created by Yoon Kwak on 9/17/20.
+//  Copyright © 2020 Yoon Kwak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
